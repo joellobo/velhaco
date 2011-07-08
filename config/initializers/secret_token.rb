e0e4cfd06bbabd29a58df8166852c024a789bce2
Joel Lobo <joellobo@gmail.com>
2011-07-07 21:49:28 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Velhaco::Application.config.secret_token = '4e7b22cdcfc1a005dbee1b91f6f33c1c6fb7dde71598c3db216d2676b3a6be1ec853ed666ad4898068211cc8eb672134d9020006c5d580eeed7d27743e9da148'
